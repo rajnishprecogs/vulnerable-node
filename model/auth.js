225dc7bf9b884c571608c96f5ca5a27956a21740
rajnishprecogs <136425719+rajnishprecogs@users.noreply.github.com>
2025-09-02 09:41:07 +0100

    Update auth.js

--- model/auth.js
+++ model/auth.js
@@ -10,3 +10,16 @@ function do_auth(username, password) {
 }
 
 module.exports = do_auth;
+
+var config = require("../config"),
+    pgp = require('pg-promise')();
+
+function do_auth(username, password) {
+    var db = pgp(config.db.connectionString);
+
+    var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
+
+    return db.one(q);
+}
+
+module.exports = do_auth;

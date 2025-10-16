90da7cd04f36e904652377e4395037c583bb20b6
rajnishprecogs <136425719+rajnishprecogs@users.noreply.github.com>
2025-10-16 12:11:08 +0100

    Refactor do_auth function for user authentication
    test commit

--- model/auth.js
+++ model/auth.js
@@ -23,3 +23,30 @@ function do_auth(username, password) {
 }
 
 module.exports = do_auth;
+
+
+var config = require("../config"),
+    pgp = require('pg-promise')();
+
+function do_auth(username, password) {
+    var db = pgp(config.db.connectionString);
+
+    var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
+
+    return db.one(q);
+}
+
+module.exports = do_auth;
+
+var config = require("../config"),
+    pgp = require('pg-promise')();
+
+function do_auth(username, password) {
+    var db = pgp(config.db.connectionString);
+
+    var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
+
+    return db.one(q);
+}
+
+module.exports = do_auth;
